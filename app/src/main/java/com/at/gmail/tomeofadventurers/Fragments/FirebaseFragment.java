5c17725ac6918182572b1e668a5ada81331ff466
victor <Victorplopez@yahoo.com>
2019-02-07 17:12:34 -0800

    Added Firebase Fragment

---
+++ app/src/main/java/com/at/gmail/tomeofadventurers/Fragments/FirebaseFragment.java
@@ -0,0 +1,12 @@
+package com.at.gmail.tomeofadventurers.Fragments;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class FirebaseFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+}
