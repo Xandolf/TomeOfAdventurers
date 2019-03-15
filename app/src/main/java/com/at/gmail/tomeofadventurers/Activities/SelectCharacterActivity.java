4f94560eafbc882fa24e8ad3d83db364ce908f5c
mikeosborne90 <mikeosborne1990@gmail.com>
2019-03-15 09:21:30 -0700

    - Create Character button now links to character creation.

--- app/src/main/java/com/at/gmail/tomeofadventurers/Activities/SelectCharacterActivity.java
+++ app/src/main/java/com/at/gmail/tomeofadventurers/Activities/SelectCharacterActivity.java
@@ -1,9 +1,12 @@
 package com.at.gmail.tomeofadventurers.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.at.gmail.tomeofadventurers.Adapters.SelectCharacterAdapter;
 import com.at.gmail.tomeofadventurers.Classes.CharacterDBAccess;
@@ -19,11 +22,13 @@ public class SelectCharacterActivity extends AppCompatActivity {
     RecyclerView selectCharacterRecyclerView;
     SelectCharacterAdapter adapter;
 
+    Button createCharButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_select_character);
         super.onCreate(savedInstanceState);
-
+//Recycler View initialization
         selectCharacterRecyclerView = findViewById(R.id.RecyclerViewSelectChar);
 
         myDatabaseAccess = CharacterDBAccess.getInstance(this);
@@ -36,6 +41,18 @@ public class SelectCharacterActivity extends AppCompatActivity {
         selectCharacterRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         selectCharacterRecyclerView.setHasFixedSize(true);
         selectCharacterRecyclerView.setAdapter(adapter);
+//Create New Character Button initializtion
+        createCharButton = findViewById(R.id.createNewCharBtn);
+
+        createCharButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent createCharacter = new Intent(getApplicationContext(), CreateCharacterActivity.class);
+                startActivity(createCharacter);
+            }
+        });
+
     }
 }
 
