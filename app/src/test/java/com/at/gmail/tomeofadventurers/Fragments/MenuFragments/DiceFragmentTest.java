e61b11094d13cdbccf0086837008488b80779af0
victor <Victorplopez@yahoo.com>
2019-04-28 17:08:29 -0700

    Added a "Health Calculator" popup

---
+++ app/src/test/java/com/at/gmail/tomeofadventurers/Fragments/MenuFragments/DiceFragmentTest.java
@@ -0,0 +1,16 @@
+package com.at.gmail.tomeofadventurers.Fragments.MenuFragments;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceFragmentTest {
+
+    @Test
+    public void onClick() {
+    }
+
+    @Test
+    public void onCreateView() {
+    }
+}
